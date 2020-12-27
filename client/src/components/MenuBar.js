cbcf68420cf24dfa42633f8d38bdcfafefec4228
Kitan Garcia <kitangar@gmail.com>
2020-12-27 13:55:32 -0800

    fix import syntax error

---
+++ client/src/components/MenuBar.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { Menu } from 'semantic-ui-react';
+
+function MenuBar() {
+  const [activeItem, setActiveItem] = useState("");
+
+  //change activeItem to item with that name
+  const handleItemClick = (e, { name }) => this.setActiveItem(name);
+
+  //don't need a render. Since this component is a function, we just return
+
+  //each menu item has a name
+  //if active property is true, it will be highlighted
+  //name (with first letter capitalized) will be rendered as the name of the tab
+
+  //take change this.handleItemClick to handleItemClick since we MenuBar is a function instead of a class
+  return (
+      <Menu pointing secondary>
+        <Menu.Item
+          name='home'
+          active={activeItem === 'home'}
+          onClick={handleItemClick}
+        />
+        <Menu.Menu position='right'>
+          <Menu.Item
+            name='login'
+            active={activeItem === 'login'}
+            onClick={handleItemClick}
+          />
+          <Menu.Item
+            name='register'
+            active={activeItem === 'register'}
+            onClick={handleItemClick}
+          />
+        </Menu.Menu>
+      </Menu>
+  )
+}
+
+export default MenuBar;
